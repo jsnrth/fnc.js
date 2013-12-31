ea502263690f50719f31116a54e41c12abc04634
Jason Roth <jasonmichaelroth@gmail.com>
2013-12-30 18:05:26 -0700

    each and eachPair tests

--- src/fnc.js
+++ src/fnc.js
@@ -11,9 +11,29 @@ var each = function(f, o){
   else if(isObject(o)){
     for(var k in o)
       if(o.hasOwnProperty(k))
-        f.call(null, o[k])
+        f.call(null, o[k]);
   }
-  return null;
+  else if(typeof o === "undefined"){
+    return function(o2){
+      return each(f, o2);
+    }
+  }
+}
+
+var eachPair = function(f, o){
+  if(isArray(o)){
+    return each(function(v, k){ return f(k, v); }, o);
+  }
+  else if(isObject(o)){
+    for(var k in o)
+      if(o.hasOwnProperty(k))
+        f.apply(null, [k, o[k]]);
+  }
+  else if(typeof o === "undefined"){
+    return function(o2){
+      return eachPair(f, o2);
+    }
+  }
 }
 
 var map = function(f, o){
@@ -28,5 +48,7 @@ var map = function(f, o){
 };
 
 module.exports = {
+  each: each,
+  eachPair: eachPair,
 	map: map
 };
--- test/fnc_test.js
+++ test/fnc_test.js
@@ -1,6 +1,76 @@
 var assert = require("assert");
 var fnc = require("../src/fnc.js");
 
+suite("each", function(){
+  test("iterates over all array elements", function(){
+    var result = [];
+    var f = function(e){ result.push(e); }
+    fnc.each(f, [1, 2, 3])
+    assert.deepEqual([1, 2, 3], result);
+  });
+
+  test("iterates over all values in an object", function(){
+    var result = [];
+    var f = function(e){ result.push(e); }
+    fnc.each(f, {one: 1, two: 2, three: 3});
+    assert.deepEqual([1, 2, 3], result);
+  });
+
+  test("auto-curries if only given a function", function(){
+    var result = [];
+    var f = fnc.each(function(e){ result.push(e); });
+    f([1, 2, 3]);
+    assert.deepEqual([1, 2, 3], result);
+  });
+
+  test("handles other things", function(){
+    var fn = function(n){ throw("should not get called"); }
+    fnc.each(fn, "foobar");
+    fnc.each(fn, 12345);
+    fnc.each(fn, true);
+    fnc.each(fn, false);
+    fnc.each(fn, null);
+    fnc.each(fn, undefined);
+  });
+});
+
+suite("eachPair", function(){
+  test("iterates over object key value pairs", function(){
+    var result = [];
+    var f = function(k, v){
+      result.push([k, v]);
+    };
+    fnc.eachPair(f, {one: 1, two: 2, three: 3});
+    assert.deepEqual([["one", 1], ["two", 2], ["three", 3]], result);
+  });
+
+  test("iterates over array index value pairs", function(){
+    var result = [];
+    var f = function(k, v){
+      result.push([k, v]);
+    };
+    fnc.eachPair(f, [1, 2, 3]);
+    assert.deepEqual([[0, 1], [1, 2], [2, 3]], result);
+  });
+
+  test("auto-curries if only given a function", function(){
+    var result = [];
+    var f = fnc.eachPair(function(k, v){ result.push([k, v]); });
+    f({one: 1, two: 2, three: 3});
+    assert.deepEqual([["one", 1], ["two", 2], ["three", 3]], result);
+  });
+
+  test("handles other things", function(){
+    var fn = function(n){ throw("should not get called"); }
+    fnc.eachPair(fn, "foobar");
+    fnc.eachPair(fn, 12345);
+    fnc.eachPair(fn, true);
+    fnc.eachPair(fn, false);
+    fnc.eachPair(fn, null);
+    fnc.eachPair(fn, undefined);
+  });
+});
+
 suite("map", function(){
   test("maps over an array", function(){
     var fn = function(n){ return n * 2 };
