9f00a30b79d74cbdaecefd92b0fec2c1fc5ddd2d
Jason Roth <jasonmichaelroth@gmail.com>
2013-12-30 17:29:51 -0700

    some refactoring

--- src/fnc.js
+++ src/fnc.js
@@ -1,17 +1,26 @@
-var map = function(f, o){
-  if(o && Array.isArray(o)){
-    var newO = [];
-    o.forEach(function(e){
-      newO.push(f(e));
-    });
-    return newO;
+var isArray = Array.isArray;
+
+var isObject = function(o){
+  return o && !isArray(o) && typeof o === "object";
+}
+
+var each = function(f, o){
+  if(isArray(o)){
+    o.forEach(f);
   }
-  else if(o && typeof o === "object") {
-    var arr = [];
+  else if(isObject(o)){
     for(var k in o)
       if(o.hasOwnProperty(k))
-        arr.push(o[k]);
-    return map(f, arr);
+        f.call(null, o[k])
+  }
+  return null;
+}
+
+var map = function(f, o){
+  if(isArray(o) || isObject(o)){
+    var a = [];
+    each(function(e){ a.push(f(e)); }, o);
+    return a;
   }
   else {
     return null;
